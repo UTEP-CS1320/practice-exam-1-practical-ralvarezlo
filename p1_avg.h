40932b395d68cd82bfcb84a2950eeb62e4c3092e
Ricardo Alvarez <35704560+ralvarezlo@users.noreply.github.com>
2020-10-10 22:27:01 -0600

    Done

--- p1_avg.h
+++ p1_avg.h
@@ -4,12 +4,12 @@ void main_avg(void) {
   int n;
   scanf("%d", &n);
   if(n == 3) {
-    int x1, x2, x3;
-    scanf("%d %d %d", &x1, &x2, &x3);
-    printf("%d\n", (x1+x2+x3) / 3);
+    float x1, x2, x3;
+    scanf("%f %f %f", &x1, &x2, &x3);
+    printf("%f\n", (x1+x2+x3) / 3);
   } else {
-    int x1, x2, x3, x4;
-    scanf("%d %d %d %d", &x1, &x2, &x3, &x4);
-    printf("%d\n", (x1+x2+x3+x4) / 3);
+    float x1, x2, x3, x4;
+    scanf("%f %f %f %f", &x1, &x2, &x3, &x4);
+    printf("%f\n", (x1+x2+x3+x4) / 4);
   }
 }
--- p2_screen.h
+++ p2_screen.h
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
 void main_screen(void) {
-  //TODO: Complete the code
+  int a, b, c, isOK = 0;
+  scanf("%d-%d-%d", &a, &b, &c);
+
+  // El Paso area
+  if(a == 915) {
+      isOK = 1;
+      if (b==701) {
+          if (c == 1000 || c == 1001) isOK = 0;
+      }
+  }
+  // Massachussets
+  else if (a == 617 && b == 253) isOK = 1;
+  // Partner numbers: 718-834-9899 and 661-112-3581
+  else if ((a==718 && b==834 && c==9899) || (a==661 && b==112 && c==3581)) isOK = 1;
+
+  printf("%s\n", (isOK)? "OK!":"BLOCKED.");
 }
